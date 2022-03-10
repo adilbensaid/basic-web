f334557ff9e1bea8749cd555d2d4c141f7236208
adilbensaid <40204984+adilbensaid@users.noreply.github.com>
2022-03-10 16:57:05 +0100

    C

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -17,13 +17,13 @@ function Contacto() {
         <div className="formulario">
           <h3>Formulario</h3>
           <form id="contact-form" method="POST">
-                <label htmlFor='name'>Full Name </label>
-                <input name="name" placeholder='Enter full name...' type="text"/>
+                <label htmlFor='name'>Nombre </label>
+                <input name="name" placeholder='Nombre...' type="text" required/>
                 <label htmlFor='email'>Email </label>
-                <input name="email" placeholder='Enter Email...' type="email"/>
-                <label htmlFor='message'>Message </label>
-                <textarea rows="6" placeholder='Enter message...' name='message required'></textarea>
-                <button type='submit'>Send Message</button>
+                <input name="email" placeholder='Email...' type="email" required/>
+                <label htmlFor='message'>Mensage </label>
+                <textarea rows="6" placeholder='Mensage...' name='message'></textarea>
+                <button type='submit'>Enviar</button>
             </form>
         </div>
       </div>
